c24831696f0156c55e75aff8cd391f60067edc18
Karthikjusme <87428710+Karthikjusme@users.noreply.github.com>
2021-07-19 11:47:36 +0530

    Add files via upload

---
+++ Backup2.ps1
@@ -0,0 +1,47 @@
+
+$rgname = "NilavembuRG_SEA"
+$loc = "southeastasia"
+$VMname = "sea-webserver2"
+$RSV = "sea-vmbackupvault"
+
+Register-AzResourceProvider -ProviderNamespace "Microsoft.RecoveryServices"
+
+New-AzRecoveryServicesVault `
+    -ResourceGroupName $rgname `
+    -Name $RSV `
+-Location $loc
+
+Get-AzRecoveryServicesVault `
+    -Name $RSV | Set-AzRecoveryServicesVaultContext
+
+    Get-AzRecoveryServicesVault `
+    -Name $RSV | Set-AzRecoveryServicesBackupProperty -BackupStorageRedundancy LocallyRedundant
+
+    $policy = Get-AzRecoveryServicesBackupProtectionPolicy     -Name "DefaultPolicy"
+
+    Enable-AzRecoveryServicesBackupProtection `
+    -ResourceGroupName $rgname `
+    -Name $VMname `
+    -Policy $policy
+
+    $backupcontainer = Get-AzRecoveryServicesBackupContainer `
+    -ContainerType "AzureVM" `
+    -FriendlyName $VMname
+
+$item = Get-AzRecoveryServicesBackupItem `
+    -Container $backupcontainer `
+    -WorkloadType "AzureVM"
+
+Backup-AzRecoveryServicesBackupItem -Item $item
+
+Get-AzRecoveryservicesBackupJob
+
+
+##Disable-AzRecoveryServicesBackupProtection -Item $item -RemoveRecoveryPoints
+#$vault = Get-AzRecoveryServicesVault -Name $RSV
+#Remove-AzRecoveryServicesVault -Vault $vault
+#Remove-AzResourceGroup -Name $rgname
+
+
+
+
